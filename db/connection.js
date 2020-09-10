746c073326eedde84e41b00e743b9fe17f225e64
Yoel Marson <yoelmarson@VE-LON-MAC-0246.home>
2020-09-10 15:50:50 +0100

    changed config

--- db/connection.js
+++ db/connection.js
@@ -2,12 +2,11 @@
 
 const Sequelize = require('sequelize');
 
-const databaseName= 'demo2'; //const databaseName = process.env.DATABASE_NAME || 'demo2';
+const databaseName= process.env.DATABASE_NAME || 'demo2';
 const databaseHost = process.env.DATABASE_HOST || 'localhost';
 const databasePort = process.env.DATABASE_PORT || 5432
-const databaseUser = 'syedasultana';
+const databaseUser = process.env.DATABASE_USER || 'platform'
 const databasePassword = process.env.DATABASE_PASSWORD || '';
-//const databaseUseSsl = process.env.DATABASE_USE_SSL !== 'false';
 const databaseUseSsl = process.env.DATABASE_USE_SSL || 'false';
 
 var opts = {
