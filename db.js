1ab54861dc5b2566fccc49c3e761d6cce4bb81b3
Yoel Marson <yoelmarson@VE-LON-MAC-0246.home>
2020-09-10 15:59:54 +0100

    streamlined files

--- db.js
+++ db.js
@@ -6,105 +6,9 @@ const dbSync = process.env.DB_SYNC || 'false';
 
 //start relations
 
-const Customer = conn.define('customer', {
-id: {
-	type: Sequelize.INTEGER, 
-	autoIncrement: true, 
-	primaryKey: true, 
-	allowNull: false
-}, 
-first_name: {
-	type: Sequelize.STRING, 
-	allowNull: false
-}, 
-last_name: {
-	type: Sequelize.STRING, 
-	allowNull: false
-}, 
-tier_id: {
-	type: Sequelize.INTEGER, 
-	allowNull: false
-}, 
-created_date: {
-	type: Sequelize.DATE, 
-	allowNull: false
-} 
- 
-}); 
-
-const Message = conn.define('message', {
-id: {
-	type: Sequelize.INTEGER, 
-	autoIncrement: true, 
-	primaryKey: true, 
-	allowNull: false
-}, 
-customer_id: {
-	type: Sequelize.INTEGER, 
-	allowNull: false
-}, 
-body: {
-	type: Sequelize.STRING, 
-	allowNull: false
-}, 
-inout: {
-	type: Sequelize.INTEGER, 
-	allowNull: false
-}, 
-message_type_id: {
-	type: Sequelize.INTEGER, 
-	allowNull: false
-}, 
-created_date: {
-	type: Sequelize.DATE, 
-	allowNull: false
-} 
- 
-}); 
-
-const Toy = conn.define('toy', {
-id: {
-	type: Sequelize.STRING, 
-	autoIncrement: true, 
-	primaryKey: true, 
-	allowNull: false
-}, 
-name: {
-	type: Sequelize.STRING, 
-	allowNull: false
-} 
- 
-}); 
-
-const Movie = conn.define('movie', {
-id: {
-	type: Sequelize.INTEGER, 
-	autoIncrement: true, 
-	primaryKey: true, 
-	allowNull: false
-}, 
-name: {
-	type: Sequelize.STRING, 
-	allowNull: false
-} 
- 
-}); 
-
-
 //end relations
 
 //start associations
-
-Customer.hasMany(Message, {
-	as: 'messages',
-	foreignKey: 'customer_id',
-	sourceKey: 'id'
-});
-Message.belongsTo(Customer, {
-	as: 'customer',
-	foreignKey: 'message_id',
-	sourceKey: 'id'
-});
 
 //end associations
 
@@ -117,10 +21,5 @@ if ( dbSync == 'true' ){
 }
 
 //start exports
-
-exports.Customer = Customer; 
-exports.Message = Message; 
-exports.Toy = Toy; 
-exports.Movie = Movie; 
 
 //end exports
--- schemas/queries.js
+++ schemas/queries.js
@@ -1,8 +1,6 @@
 const { GraphQLString, GraphQLObjectType, GraphQLInt, GraphQLID, GraphQLList } = require("graphql");
 //start imports
 
-const { Customer, Message, Toy, Movie } = require('../db');
-const { CustomerType, MessageType, ToyType, MovieType } = require('./types');
 //end imports
 
 const RootQuery = new GraphQLObjectType({
@@ -12,55 +10,6 @@ const RootQuery = new GraphQLObjectType({
 
 //start queries
 
-customers: {
-  type: new GraphQLList(CustomerType),
-  args: { },
-  resolve(parentValue, args) {
-      return Customer.findAll();
-  }
-},
-messages: {
-  type: new GraphQLList(MessageType),
-  args: { },
-  resolve(parentValue, args) {
-      return Message.findAll();
-  }
-},
-message: {
-  type: MessageType,
-  args: { id: { type: GraphQLID } },
-  resolve(parentValue, args) {
-    return Message.findByPk(args['id']);
-  }
-},
-toys: {
-  type: new GraphQLList(ToyType),
-  args: { },
-  resolve(parentValue, args) {
-      return Toy.findAll();
-  }
-},
-toys_By_product_version_id: {
-  type: new GraphQLList(ToyType),
-  args: { product_version_id: { type: GraphQLID } },
-  resolve(parentValue, args) {
-    return Toy.findAll( { where: args} );
-  }
-},
-movies: {
-  type: new GraphQLList(MovieType),
-  args: { },
-  resolve(parentValue, args) {
-      return Movie.findAll();
-  }
-},
-movies_By_product_version_id: {
-  type: new GraphQLList(MovieType),
-  args: { product_version_id: { type: GraphQLID } },
-  resolve(parentValue, args) {
-    return Movie.findAll( { where: args} );
-  }
-}
 //end queries
   },
 });
