5ad4e83cae148e71e9d5fa1ae22d64a0e263747b
Yoel Marson <yoelmarson@VE-LON-MAC-0246.home>
2020-09-10 16:02:42 +0100

    modified default db connections

--- db/connection.js
+++ db/connection.js
@@ -2,10 +2,10 @@
 
 const Sequelize = require('sequelize');
 
-const databaseName= process.env.DATABASE_NAME || 'demo2';
+const databaseName= process.env.DATABASE_NAME || 'demo';
 const databaseHost = process.env.DATABASE_HOST || 'localhost';
 const databasePort = process.env.DATABASE_PORT || 5432
-const databaseUser = process.env.DATABASE_USER || 'platform'
+const databaseUser = process.env.DATABASE_USER || 'demouser'
 const databasePassword = process.env.DATABASE_PASSWORD || '';
 const databaseUseSsl = process.env.DATABASE_USE_SSL || 'false';
 
