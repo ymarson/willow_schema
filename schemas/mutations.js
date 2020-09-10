66eeb133434f2bdf672673e5d1d00fa1b5b1a440
syeda sultana <syedamahasultana@gmail.com>
2020-09-10 11:33:41 +0100

    added placeholders

--- schemas/mutations.js
+++ schemas/mutations.js
@@ -1,14 +1,66 @@
-const graphql = require("graphql");
-const Sequelize = require("sequelize");
-const {  } = require("../db");
+
+const graphql = require('graphql');
+const Sequelize = require('sequelize')
+const { Customer } = require('../db');
+const { Toy } = require('../db');
 const { GraphQLObjectType, GraphQLInt, GraphQLString, GraphQLBoolean, GraphQLList } = graphql;
-const {  } = require("./types");
+
+//start imports
+
+
+const { CustomerType } = require('./types');
+const { MessageType } = require('./types');
+const { ToyType } = require('./types');
+const { MovieType } = require('./types');
+//end imports
+
+//start mutation
 
 const RootMutation = new GraphQLObjectType({
-  name: "RootMutationType",
-  type: "Mutation",
+  name: 'RootMutationType',
+  type: 'Mutation',
   fields: {
+    addCustomer: {
+      type: CustomerType,
+       args: {
+        name: { type: GraphQLString }
+       },
+       resolve(parentValue, args) {
+        Customer.create({id: args['id'], first_name: args['first_name'], last_name: args['last_name'], tier_id: args['tier_id'], created_date: Sequelize.fn('NOW')});
+       }
+   },
+    addMessage: {
+      type: MessageType,
+       args: {
+        name: { type: GraphQLString }
+       },
+       resolve(parentValue, args) {
+        Message.create({id: args['id'], customer_id: args['customer_id'], body: args['body'], inout: args['inout'], message_type_id: args['message_type_id'], created_date: Sequelize.fn('NOW')});
+       }
+   },
+    addToy: {
+      type: ToyType,
+       args: {
+        name: { type: GraphQLString }
+       },
+       resolve(parentValue, args) {
+        Toy.create({id: args['id'], name: args['name']});
+       }
+   },
+    addMovie: {
+      type: MovieType,
+       args: {
+        name: { type: GraphQLString }
+       },
+       resolve(parentValue, args) {
+        Movie.create({id: args['id'], name: args['name']});
+       }
+    }
+
   }
 });
 
+
+
 exports.mutation = RootMutation;
+//end mutation
