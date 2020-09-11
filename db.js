615a2a8a08efa6dfa8457c5273474ddbd1c3c7d4
syeda sultana <syedamahasultana@gmail.com>
2020-09-11 10:05:16 +0100

    fixed indenting for db.js

--- db.js
+++ db.js
@@ -6,11 +6,72 @@ const dbSync = process.env.DB_SYNC || 'false';
 
 //start relations
 
+const Customer = conn.define('customer', {
+  id: {
+	type: Sequelize.INTEGER, 
+	autoIncrement: true, 
+	primaryKey: true, 
+	allowNull: false
+  }, 
+  first_name: {
+	type: Sequelize.STRING, 
+	allowNull: false
+  }, 
+  last_name: {
+	type: Sequelize.STRING, 
+	allowNull: false
+  }, 
+  tier_id: {
+	type: Sequelize.INTEGER, 
+	allowNull: false
+  }, 
+  created_date: {
+	type: Sequelize.DATE, 
+	allowNull: false
+   } 
+ 
+}); 
+
+const Message = conn.define('message', {
+  id: {
+	type: Sequelize.INTEGER, 
+	autoIncrement: true, 
+	primaryKey: true, 
+	allowNull: false
+  }, 
+  customer_id: {
+	type: Sequelize.INTEGER, 
+	allowNull: false
+  }, 
+  body: {
+	type: Sequelize.STRING, 
+	allowNull: false
+  }, 
+  inout: {
+	type: Sequelize.INTEGER, 
+	allowNull: false
+  }, 
+  message_type_id: {
+	type: Sequelize.INTEGER, 
+	allowNull: false
+  }, 
+  created_date: {
+	type: Sequelize.DATE, 
+	allowNull: false
+   } 
+ 
+}); 
+
 
 //end relations
 
 //start associations
 
+Customer.hasMany(Message, {
+	as: 'messages',
+	foreignKey: 'customer_id',
+	sourceKey: 'id'
+});
 
 //end associations
 
@@ -24,5 +85,7 @@ if ( dbSync == 'true' ){
 
 //start exports
 
+exports.Customer = Customer; 
+exports.Message = Message; 
 
 //end exports
