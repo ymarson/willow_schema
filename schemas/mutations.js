9eb9c7a69721da1d4a20964c7858cdd7b8e41bf6
Syeda Sultana <37453322+syedasultana@users.noreply.github.com>
2020-09-11 14:01:03 +0100

    Cleaned for demo

--- schemas/mutations.js
+++ schemas/mutations.js
@@ -13,15 +13,7 @@ const RootMutation = new GraphQLObjectType({
   name: 'RootMutationType',
   type: 'Mutation',
   fields: {
-    addCustomer: {
-      type: CustomerType,
-       args: {
-        name: { type: GraphQLString }
-       },
-       resolve(parentValue, args) {
-        Customer.create({id: args['id'], first_name: args['first_name'], last_name: args['last_name'], tier_id: args['tier_id'], created_date: Sequelize.fn('NOW')});
-       }
-   }
+    
   }
 });
 
