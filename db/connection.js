558230c9f2ca7ab1b2747c0c4e7d556437332f26
Syeda Sultana <37453322+syedasultana@users.noreply.github.com>
2020-10-16 14:00:26 +0100

    added password

--- db/connection.js
+++ db/connection.js
@@ -6,7 +6,7 @@ const databaseName= process.env.DATABASE_NAME || 'demo';
 const databaseHost = process.env.DATABASE_HOST || 'localhost';
 const databasePort = process.env.DATABASE_PORT || 5432
 const databaseUser = process.env.DATABASE_USER || 'demouser'
-const databasePassword = process.env.DATABASE_PASSWORD || '';
+const databasePassword = process.env.DATABASE_PASSWORD || 'password';
 const databaseUseSsl = process.env.DATABASE_USE_SSL || 'false';
 
 var opts = {
